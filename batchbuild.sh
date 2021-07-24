b036d85ea21acb259971640dbddc9e683068999d
kaxxa123 <axzammit@hotmail.com>
2021-07-24 18:07:02 +0200

    added nodegw to batch build

--- batchbuild.sh
+++ batchbuild.sh
@@ -4,15 +4,24 @@ echo "=================="
 echo "Building zkConsent"
 echo "=================="
 
+mkdir -p ./build/bin
+mkdir    ./build/include
+mkdir ./snarks/build
 
 echo
 echo ">> Building SNARKs"
-mkdir ./build
-mkdir ./snarks/build
 cmake -S ./snarks -B ./snarks/build
 make -C  ./snarks/build
-cp ./snarks/build/snarkhlp/libsnarkhlp.a  ./build
-cp ./snarks/build/test/MYSNARK  ./build
+
+cp ./snarks/build/snarkhlp/libsnarkhlp.a    ./build/bin
+cp ./snarks/build/test/MYSNARK              ./build/bin
+cp ./snarks/snarkhlp/prfxxx.hpp             ./build/include
+
+echo
+echo ">> Building nodegw"
+node-gyp rebuild -C ./nodegw
+
+cp ./nodegw/build/Release/nodegw.node       ./build/bin
 
 echo
 echo "Ready!"
--- batchclean.sh
+++ batchclean.sh
@@ -6,8 +6,12 @@ echo "========================================"
 echo
 
 echo
-echo ">> Deleting SNARK build dir"
+echo ">> Deleting SNARK build"
 rm ./snarks/build -rf
+
+echo
+echo ">> Deleting nodegw build"
+node-gyp clean -C ./nodegw
 
 echo
 echo ">> Deleting main build dir"
