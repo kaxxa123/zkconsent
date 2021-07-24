9193b0496c749480ee7761a7b28d79296a527c1c
kaxxa123 <axzammit@hotmail.com>
2021-07-24 18:08:14 +0200

    intial support for nodegw to wrap snarkhlp

--- nodegw/binding.gyp
+++ nodegw/binding.gyp
@@ -2,7 +2,9 @@
   "targets": [
     {
       "target_name": "nodegw",
-      "sources": [ "src/snark2node.cc" ]
+      "sources": [ "src/snark2node.cc" ],
+      "libraries": ["<!(pwd)/../build/bin/libsnarkhlp.a"],      
+      "include_dirs" : [ "<!(pwd)/../build/include" ]
     }
   ]
 }
--- nodegw/src/snark2node.cc
+++ nodegw/src/snark2node.cc
@@ -1,8 +1,30 @@
 // snark2node.cc using N-API
+
+#include <stdlib.h>
+#include <iostream>
+#include <algorithm>
+
 #include <node_api.h>
+#include "prfxxx.hpp"
 
 
 namespace demo {
+
+    const char* StubPRFapk() {
+        InitSnarks();
+
+        const char* ask = "0F000000000000FF00000000000000FF00000000000000FF00000000000000FF";
+        std::string apk_expected = "2390c9e5370be7355f220b29caf3912ef970d828b73976ae9bfeb1402ce4c1f9";
+        std::string apk = PRFapk(ask);
+
+        std::transform(apk.begin(), apk.end(), apk.begin(), ::toupper);
+        std::transform(apk_expected.begin(), apk_expected.end(), apk_expected.begin(), ::toupper);
+
+        if (apk.compare(apk_expected) != 0)
+            return nullptr;
+
+        return apk.c_str();
+    }
 
     napi_value Method(napi_env env, napi_callback_info args) {
         napi_value greeting;
