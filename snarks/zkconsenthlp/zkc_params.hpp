c13706bc6d3b381118c18dc8d5f8e9161ec247c5
kaxxa123 <axzammit@hotmail.com>
2021-08-12 14:50:52 +0200

    studying the wrapping of public inputs into a hash for a single zkp verification input

--- snarks/zkconsenthlp/zkc_params.hpp
+++ snarks/zkconsenthlp/zkc_params.hpp
@@ -15,6 +15,12 @@ using VKT           = libsnark::r1cs_gg_ppzksnark_verification_key<pp>;
 using KeypairT      = libsnark::r1cs_gg_ppzksnark_keypair<pp>;
 using ProofT        = libsnark::r1cs_gg_ppzksnark_proof<pp>;
 
+///home/alex/zkconsent/snarks/depends/zeth/zeth_config.h.in
+///home/alex/zkconsent/snarks/depends/zeth/libzeth/snarks/groth16/groth16_snark.hpp
+// using SnarkT        = groth16_snark<pp>;
+// using APIHandlerT   = groth16_api_handler<pp>;
+
+
 const size_t ZKC_TreeDepth      = libzeth::ZETH_MERKLE_TREE_DEPTH;
 const size_t ZKC_STUDYID_SIZE   = libzeth::ZETH_V_SIZE;
 const size_t ZKC_CHOICE_SIZE    = 8;
